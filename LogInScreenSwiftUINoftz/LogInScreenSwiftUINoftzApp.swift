415c165d3a2e241958eb5130a278fcbeb248171c
esnoftz <esnoftz@gmail.com>
2025-04-11 07:33:56 -0500

    Initial Commit

---
+++ LogInScreenSwiftUINoftz/LogInScreenSwiftUINoftzApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInScreenSwiftUINoftzApp.swift
+//  LogInScreenSwiftUINoftz
+//
+//  Created by EVANGELINE NOFTZ on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LogInScreenSwiftUINoftzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LogInScreenSwiftUINoftzTests/LogInScreenSwiftUINoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  LogInScreenSwiftUINoftzTests.swift
+//  LogInScreenSwiftUINoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 4/11/25.
+//
+
+import Testing
+@testable import LogInScreenSwiftUINoftz
+
+struct LogInScreenSwiftUINoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
